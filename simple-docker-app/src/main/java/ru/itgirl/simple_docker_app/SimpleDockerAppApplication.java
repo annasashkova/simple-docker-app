422f3eac1a184fa301392fcd1be0b1dec8b9ec90
annasashkova <avlaada99@gmail.com>
2025-07-20 21:26:44 +0300

    week 27

---
+++ simple-docker-app/src/main/java/ru/itgirl/simple_docker_app/SimpleDockerAppApplication.java
@@ -0,0 +1,12 @@
+package ru.itgirl.simple_docker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDockerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleDockerAppApplication.class, args);
+    }
+}
